eb95d58d1e6145154476d2c3cade08cc37a53dad
Dharnish-BM <dharnish.bm.2005@gmail.com>
2025-11-21 00:10:50 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true // important: allow http-only cookie auth with backend
 });
 export default api;
